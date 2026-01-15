75115f7ae3f4f9ffe377179a8de3e53da818a6b2
bartix259 <bartek.masiak259@gmail.com>
2026-01-15 21:26:38 +0100

    client: cursor move throttling

--- client/lua/collab/init.lua
+++ client/lua/collab/init.lua
@@ -10,20 +10,36 @@ local PORT = 8080
 
 local function enable_cursor_tracking()
   local group = vim.api.nvim_create_augroup("CollabActive", { clear = true })
+  local timer = assert((vim.uv or vim.loop).new_timer())
+  local THROTTLE_MS = 200
+  local pending_update = false
+
+  local function send_cursor_payload()
+    if not transport.client then return end
+    local buf = vim.api.nvim_get_current_buf()
+    if vim.bo[buf].buftype ~= "" then return end
+    local path = vim.api.nvim_buf_get_name(buf)
+    if path == "" then return end
+    path = vim.fn.fnamemodify(path, ":.")
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local msg = protocol.cursor(state.client_id, path, cursor[1] - 1, cursor[2])
+    transport.send(msg)
+  end
 
   vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
     group = group,
     callback = function()
-      if not transport.client then return end
-
-      local buf = vim.api.nvim_get_current_buf()
-      if vim.bo[buf].buftype ~= "" then return end
-
-      local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":.")
-      local cursor = vim.api.nvim_win_get_cursor(0) -- (1-indexed row, 0-indexed col)
-
-      local msg = protocol.cursor(state.client_id, path, cursor[1] - 1, cursor[2])
-      transport.send(msg)
+      if timer:is_active() then
+        pending_update = true
+        return
+      end
+      send_cursor_payload()
+      timer:start(THROTTLE_MS, 0, vim.schedule_wrap(function()
+        if pending_update then
+          send_cursor_payload()
+          pending_update = false
+        end
+      end))
     end
   })
 end
